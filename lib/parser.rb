2b6b6998400b34ab8d5df791f1ce41291cd14b4e
DrewDalmedo <andrewdalmedo@gmail.com>
2020-05-08 16:52:55 -0400

    Added proper functionality to self.save_jobs method

--- lib/parser.rb
+++ lib/parser.rb
@@ -43,13 +43,24 @@ class Parser
     end
 
     # entries should be either a hash or an array of hashes which contain the job postings the user wants to save
-    def self.save_entries(entries:, user:)
-        GITHUB_POSITION_LINK = "https://jobs.github.com/positions/"
+    def self.save_jobs(jobs:, user:)
         # we want to pull close to all the data from the :githubjobs_url link
-        entries.each do |entry|
-            temp_hash = {}
-            parsed_json = JSON.parse(open("#{entry[:githubjobs_url]}.json").read)
+        jobs.each do |job|
+            job_posting = JSON.parse(open("#{job[:githubjobs_url]}.json").read)
+            
+            hash = {}
 
+            hash[:title] = job_posting["title"]
+            hash[:type] = job_posting["type"]
+            hash[:company] = job_posting["company"]
+            hash[:githubjobs_url] = job_posting["githubjobs_url"]
+            hash[:company_url] = job_posting["company_url"]
+            hash[:created_at] = job_posting["created_at"]
+            hash[:location] = job_posting["location"]
+            hash[:description] = job_posting["description"]
+            hash[:how_to_apply] = job_posting["how_to_apply"]
+                
+            user.save_job( Job.new(hash) )
             
             #user.saved_jobs << Job.new(entry)
         end
