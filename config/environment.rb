1dc8d1771a1e591e69d798543d0f405ee1b89a94
DrewDalmedo <andrewdalmedo@gmail.com>
2020-05-11 12:40:44 -0400

    Removed unneeded requires

--- config/environment.rb
+++ config/environment.rb
@@ -2,13 +2,6 @@
 require 'bundler/setup'
 Bundler.require(:default, :development)
 
-# gem requirements
-require 'pry'
-require 'json'
-require 'open-uri'
-require 'awesome_print'
-require 'sanitize'
-
 # local file requirements
 require_relative '../lib/jobfindercontroller.rb' # require all files in lib after you've made them!
 require_relative '../lib/parser.rb'
