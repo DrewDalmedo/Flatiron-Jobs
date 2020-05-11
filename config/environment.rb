5d027a3238ab80a318592d15d29a8db1f173632a
DrewDalmedo <andrewdalmedo@gmail.com>
2020-05-11 12:56:40 -0400

    Added gem requirements

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,10 @@
 require 'bundler/setup'
 Bundler.require(:default, :development)
 
+# gem requirements
+require 'open-uri'
+require 'json'
+
 # local file requirements
 require_relative '../lib/jobfindercontroller.rb' # require all files in lib after you've made them!
 require_relative '../lib/parser.rb'
