2b1757ef059ff7d5f416d8efa1e4e59e2561a781
DrewDalmedo <andrewdalmedo@gmail.com>
2020-05-11 12:33:17 -0400

    Added print_jobs method

--- lib/user.rb
+++ lib/user.rb
@@ -8,7 +8,34 @@ class User
         @wants_type_of_job = wants_type_of_job
     end
 
+    '''
+    def saved_jobs()
+        Job.all.select {|job| job.saved_by_users.include?(self)}
+    end
+    '''
+
     def save_job(job)
         @saved_jobs << job
+    end
+
+    def print_jobs()
+
+        if @saved_jobs.size == 0
+            puts "#{"You have not saved any jobs!".red}"
+        else
+            @saved_jobs.each do |job|
+                puts "#{job.title.greenish} : #{job.company.blue}\n\n"
+                puts "#{job.description}"
+                #don't print if last job in array
+                puts "-------------------------" if @saved_jobs.index(job) != @saved_jobs.size-1 
+            end
+        end
+
+
+        '''
+        puts "#{results[user_input-1][:title].greenish} : #{results[user_input-1][:company].blue}\n\n"
+        puts results[user_input-1][:description]
+        puts "-------------------------"
+        '''
     end
 end
